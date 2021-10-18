6e399d9bb7bcec1fc83c937d17bc1089900defd0
zhuchentong <2037630@163.com>
2021-10-18 16:49:17 +0800

    'update'

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'dart:io';
 import 'dart:async';
 
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:ffi/ffi.dart';
+import 'package:nanoid/non_secure.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:win32/win32.dart';
 import 'package:system_tray/system_tray.dart';
 import 'package:path/path.dart' as p;
@@ -10,24 +14,59 @@ import 'package:window_manager/window_manager.dart';
 
 var appWindow = WindowManager.instance;
 
+const TASK_KEY = 'TASK_MINUTE';
+Timer? timer;
+
+const DownloadURL = "https://source.unsplash.com/1920x1080";
 main() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+
   WidgetsFlutterBinding.ensureInitialized();
-
+  // 设置系统托盘
   setupSystemTray();
-
-  runApp(const WallpaperApp());
-
   // 隐藏默认窗口
   WindowManager.instance.hide();
+
+  var minutes = prefs.getInt(TASK_KEY);
+
+  if (minutes != null && minutes > 0) {
+    setTimerTask(minutes);
+  }
+
+  runApp(const WallpaperApp());
 }
 
-setWallpaper() {
-  SystemParametersInfo(20, 1, 'D:/test.jpg'.toNativeUtf16(), 0x2);
+setWallpaper() async {
+  print("开始更新壁纸");
+  var path = await downloadWallpaper();
+
+  SystemParametersInfo(20, 1, path.toNativeUtf16(), 0x2);
+  print(path);
+}
+
+setTimerTask(int minute) {
+  if (timer != null) {
+    timer?.cancel();
+  }
+
+  var duration = Duration(minutes: minute);
+  timer = Timer.periodic(duration, (timer) {
+    setWallpaper();
+  });
+}
+
+Future<String> downloadWallpaper() async {
+  // 获取文件路径
+  var path = await getApplicationSupportDirectory();
+  var file = File(p.joinAll([path.path, "${nanoid()}.jpg"]));
+
+  await Dio().download(DownloadURL, file.path);
+  return file.path;
 }
 
 Future<void> setupSystemTray() async {
   String path;
-
+  // 设置托盘图标
   if (Platform.isWindows) {
     path = p.joinAll([
       p.dirname(Platform.resolvedExecutable),
@@ -45,48 +84,86 @@ Future<void> setupSystemTray() async {
   }
 
   var systemTray = SystemTray();
+  // 初始化托盘
+  await systemTray.initSystemTray("每刻壁纸", iconPath: path);
 
-  // We first init the systray menu and then add the menu entries
-  await systemTray.initSystemTray("system tray",
-      iconPath: path, toolTip: "How to use system tray with Flutter");
-
-  await systemTray.setContextMenu(
-    [
-      MenuItem(
-        label: 'Show',
-        onClicked: () {
-          // appWindow.show();
-        },
-      ),
-      MenuSeparator(),
-      SubMenu(
-        label: "SubMenu",
-        children: [
-          MenuItem(
-            label: 'SubItem1',
-            enabled: false,
-            onClicked: () {
-              print("click SubItem1");
-            },
-          ),
-          MenuItem(label: 'SubItem2'),
-          MenuItem(label: 'SubItem3'),
-        ],
-      ),
-      MenuSeparator(),
-      MenuItem(
-        label: 'Exit',
-        onClicked: () {
-          // appWindow.close();
-        },
-      ),
-    ],
-  );
+  await setupSystemTrayState(systemTray);
 
   // handle system tray event
   systemTray.registerSystemTrayEventHandler((eventName) {
     print("eventName: $eventName");
   });
+}
+
+setupSystemTrayState(SystemTray systemTray) async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+
+  var minutes = prefs.getInt(TASK_KEY);
+  print(minutes);
+  return systemTray.setContextMenu(
+    [
+      MenuItem(
+        label: '更新壁纸',
+        onClicked: () {
+          setWallpaper();
+        },
+      ),
+      MenuSeparator(),
+      SubMenu(
+        label: "自动更新",
+        children: [
+          MenuItem(
+            label: "关闭${minutes == null ? '(当前)' : ''}",
+            onClicked: () {
+              prefs.clear();
+              timer?.cancel();
+              setupSystemTrayState(systemTray);
+            },
+          ),
+          MenuItem(
+            label: "1分钟${minutes == 1 ? '(当前)' : ''}",
+            onClicked: () {
+              print('11');
+              prefs.setInt(TASK_KEY, 1);
+              setTimerTask(1);
+              setupSystemTrayState(systemTray);
+            },
+          ),
+          MenuItem(
+            label: "15分钟${minutes == 15 ? '(当前)' : ''}",
+            onClicked: () {
+              prefs.setInt(TASK_KEY, 15);
+              setTimerTask(15);
+              setupSystemTrayState(systemTray);
+            },
+          ),
+          MenuItem(
+            label: "30分钟${minutes == 30 ? '(当前)' : ''}",
+            onClicked: () {
+              prefs.setInt(TASK_KEY, 30);
+              setTimerTask(30);
+              setupSystemTrayState(systemTray);
+            },
+          ),
+          MenuItem(
+            label: "60分钟${minutes == 60 ? '(当前)' : ''}",
+            onClicked: () {
+              prefs.setInt(TASK_KEY, 60);
+              setTimerTask(60);
+              setupSystemTrayState(systemTray);
+            },
+          ),
+        ],
+      ),
+      MenuSeparator(),
+      MenuItem(
+        label: '退出',
+        onClicked: () {
+          exit(0);
+        },
+      ),
+    ],
+  );
 }
 
 class WallpaperApp extends StatelessWidget {
@@ -98,7 +175,7 @@ class WallpaperApp extends StatelessWidget {
       home: Scaffold(
         body: Container(
           child: Row(
-            children: [Text('test')],
+            children: [],
           ),
         ),
       ),
